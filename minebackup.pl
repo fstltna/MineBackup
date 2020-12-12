d0311fc6f1f5f9b33f95a81f897e9e8ff1b2a563
Minetest Owner <mtowner@sara.minecity.online>
2020-12-11 18:56:37 -0800

    Remove testIRC

--- minebackup.pl
+++ minebackup.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 #use strict;
-use IO::Socket;
+#use IO::Socket;
 #use warnings;
 #use AnyEvent;
 #use AnyEvent::IRC::Client;
@@ -33,35 +33,6 @@ sub debugPrint
 	{
 		print "$_[0]";
 	}
-}
-
-sub testIRC
-{
-	my $irc = Mojo::IRC->new(
-            nick => 'BackupScript',
-            user => 'Minetest Backup Script',
-            server => 'irc.freenode.net:6667',
-            name => 'Minecity Backup Script',
-          );
- 
-	$irc->on(irc_join => sub {
-		my($self, $message) = @_;
-		warn "yay! i joined $message->{params}[0]";
-	});
- 
-	$irc->on(irc_privmsg => sub {
-		my($self, $message) = @_;
-		say $message->{prefix}, " said: ", $message->{params}[1];
-	});
- 
-	$irc->connect(sub {
-		my($irc, $err) = @_;
-		return warn $err if $err;
-		$irc->write(join => '##changeme');
-		$irc->write('Test Message');
-	});
- 
-	Mojo::IOLoop->start;
 }
 
 if (-f $BACKUP_CONFIG)
@@ -134,59 +105,33 @@ sub DoWarn
 {
 	print "Doing a delay of $BACKUP_DELAY minutes\n";
 
-	# Connect to the IRC server.
-	my $sock = new IO::Socket::INET(PeerAddr => $IRC_SERVER,
-					PeerPort => $IRC_PORT,
-					Proto => 'tcp') or die "Can't connect to IRC server '$IRC_SERVER' on port '$IRC_PORT'\n";
+        my $irc = Mojo::IRC->new(
+            nick => 'BackupScript',
+            user => 'Minetest Backup Script',
+            server => 'irc.freenode.net:6667',
+            name => 'Minecity Backup Script',
+          );
 
-	# Log on to the server.
-	print $sock "NICK $IRC_NICK\r\n";
-	#print $sock "USER $IRC_LOGINNAME 8 * :Perl IRC Hacks Robot\r\n";
-	print $sock "USER $IRC_LOGINNAME 8 * :$IRC_LOGINNAME\r\n";
+        $irc->on(irc_join => sub {
+                my($self, $message) = @_;
+                warn "yay! i joined $message->{params}[0]";
+        });
 
-	# Read lines from the server until it tells us we have connected.
-	while (my $input = <$sock>)
-	{
-		my $SawIt = chop($sock);
-		debugPrint("$SawIt\n");
+        $irc->on(irc_privmsg => sub {
+                my($self, $message) = @_;
+                say $message->{prefix}, " said: ", $message->{params}[1];
+        });
 
-		# Check the numerical responses from the server.
-		if ($input =~ /004/)
-		{
-			# We are now logged in.
-			last;
-		}
-		elsif ($input =~ /433/)
-		{
-			die "Nickname '$IRC_NICK' is already in use.";
-		}
-	}
+        $irc->connect(sub {
+                my($irc, $err) = @_;
+                return warn $err if $err;
+                $irc->write(join => '##changeme');
+                $irc->write('Test Message');
+        });
 
-	# Join the channel.
-	print($sock "JOIN $IRC_CHAN\r\n");
-	print ("joined channel: 'JOIN $IRC_CHAN'\n");
-print $sock "Warning - the minetest game is about to run a backup. You have $BACKUP_DELAY minutes to finish saving your changes.\r\n";
-	# Keep reading lines from the server.
-	while (my $input = <$sock>)
-	{
-		debugPrint("SawLine\n");
-		chop $input;
-		if ($input =~ /^PING(.*)$/i)
-		{
-			# We must respond to PINGs to avoid being disconnected.
-			print $sock "PONG $1\r\n";
-		}
-		else
-		{
-			if ($SHOW_SERVER_OUTPUT eq "true")
-			{
-				# Print the raw line received by the bot.
-				print "$input\n";
-			}
-		}
+        Mojo::IOLoop->start;
+
 		print $sock "Warning - the minetest game is about to run a backup. You have $BACKUP_DELAY minutes to finish saving your changes.\r\n";
-		last;
-	}
 	debugPrint"About to sleep\n";
 	sleep($BACKUP_DELAY * 60);
 }
@@ -201,8 +146,7 @@ print "=========================\n";
 
 if ($DOING_DELAY eq "true")
 {
-	testIRC();
-	#DoWarn();
+	DoWarn();
 }
 
 exit 0;
